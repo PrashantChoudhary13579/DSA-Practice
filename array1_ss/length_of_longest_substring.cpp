54f882013e9d4b7f261bb850297b6028c679cf81
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2026-02-28 23:55:57 +0530

    length of the longest common substring

---
+++ array1_ss/length_of_longest_substring.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        unordered_map<char, int> mp;   
+        int left = 0;
+        int maxLen = 0;
+
+        for(int right = 0; right < s.size(); right++) {
+
+            if(mp.count(s[right]) && mp[s[right]] >= left) {
+                left = mp[s[right]] + 1;
+            }
+            mp[s[right]] = right;
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+};
+
+int main() {
+    
+    Solution obj;    
+    string s = "abcabcbb";
+    cout << obj.lengthOfLongestSubstring(s);
+    return 0;
+}
