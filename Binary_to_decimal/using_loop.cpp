7e74d18f8068ef2df8c02d687f9d5f62f81af901
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2026-03-24 23:14:15 +0530

    Converting binary value into decimal value

---
+++ Binary_to_decimal/using_loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+using namespace std;
+int main(){
+    int num =  101101;
+    int val = 0;
+    int base = 1;
+    int temp = num;
+    while(temp){
+        int rem = temp % 10;
+        temp = temp /10;
+        val += rem * base;
+        base *= 2;
+    }
+    cout<<"Decimal value: "<<val<<endl;
+    return 0;
+}
